fe049cc2dc6a363447b427d20a4401c5b25eebe8
Kwan Voong <kwan.voong@ensimag.grenoble-inp.fr>
2018-04-10 11:56:57 +0200

    try to display the questions with the answers

--- src/RunningQuiz/RunningQuiz.js
+++ src/RunningQuiz/RunningQuiz.js
@@ -10,6 +10,7 @@ class RunningQuiz extends Component {
         this.state = {
             askedQuestions : modelInstance.getAskedQuestions(),
             artists: modelInstance.getArtists(),
+            songs: []
         }
         modelInstance.addObserver(this);
     }
@@ -24,29 +25,43 @@ class RunningQuiz extends Component {
 
     }
 
-    nextQuestion = function(){
+    nextQuestion = function() {
         let question = modelInstance.getRandomQuestion();
         let artists = this.state.artists;
-        if (artists.length !== 4 || artists === undefined){
+        if (artists.length !== 4 || artists === undefined) {
             return;
         }
         modelInstance.addAskedQuestion(question);
         console.log("added question");
 
         console.log(artists);
-        let goodArtist = artists[modelInstance.getRandomInt(artists.length)][0]
+        let goodArtist = artists[modelInstance.getRandomInt(artists.length)][0];
         console.log(goodArtist);
+        let songs;
+        let goodSong;
+        modelInstance.getSongs(goodArtist.artist_name).then((data) => {
+            songs = data;
+            goodSong = data[modelInstance.getRandomInt(data.length)];
+            console.log(songs[1]);
+            console.log(this.displayQuestion(question, goodArtist, goodSong));
+            let obj = this.displayQuestion(question, goodArtist, goodSong);
+            return <div>{obj.quest} {obj.param} <br/> {obj.ans}</div>
+        });
+    }
 
+    displayQuestion = function(question, goodArtist, goodSong){
+    let questionParam;
+    let answers;
         switch(question.id){
             case 0:
                 //Pick up the name of the song goodSong
                 //Propose the 4 singers
-                //let questionParam = goodSong.track_name;
-                //let answers = <div>{artists[0].artist_name} <br/>
-                //    {artists[1].artist_name} <br/>
-                //    {artists[2].artist_name} <br/>
-                //    {artists[3].artist_name}
-                //</div>
+                questionParam = goodSong.track_name;
+                answers = <div>{this.state.artists[0].artist_name} <br/>
+                    {this.state.artists[1].artist_name} <br/>
+                    {this.state.artists[2].artist_name} <br/>
+                    {this.state.artists[3].artist_name}
+                </div>
                 break;
             case 1:
                 //Pick up the id of the song goodSong
@@ -65,10 +80,11 @@ class RunningQuiz extends Component {
             default:
                 break;
         }
-        return question.question;
+        return {quest : question.question, param: questionParam, ans : answers};
     }
 
     render() {
+        console.log(this.nextQuestion())
         return (
             <div className="RunningQuiz">
                 <Sidebar model={modelInstance} store={this.props.store}/>
