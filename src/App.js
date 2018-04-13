76d8b729c69e218cae9aaeb8d9aa81b5ab9ef61a
Jordan Rye <jordanrye1@gmail.com>
2018-04-13 14:27:12 +0200

    firebase access

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import SelectedArtists from "./SelectedArtists/SelectedArtists";
 import RunningQuiz from "./RunningQuiz/RunningQuiz";
 import { modelInstance } from './data/Model'
 import quizApp from './reducer/reducers';
-
+import * as firebase from 'firebase';
 
 const store= createStore(quizApp);
 
-
+var firebaseConfig = {
+    apiKey: "AIzaSyB91MtsueeW8ij89tz38Uu9HSdKVypIRp8",
+    authDomain: "dh2642-aa0f4.firebaseapp.com",
+    databaseURL: "https://dh2642-aa0f4.firebaseio.com",
+    storageBucket: "dh2642-aa0f4.appspot.com",
+};
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
-    
+
     render() {
         return (
             <div className="App">
